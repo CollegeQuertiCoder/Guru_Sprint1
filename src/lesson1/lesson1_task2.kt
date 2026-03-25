25867c6a4847464472fe9b1d7c988250b8ba2446
1 <gitigitigiriir@gmail.com>
2026-03-25 12:53:34 +0300

    GS-1-3 maked clear commit

--- src/lesson1/lesson1_task2.kt
+++ src/lesson1/lesson1_task2.kt
@@ -4,6 +4,5 @@ fun main() {
     val applePrice : Double = 70000.0
     val applesAmount : Int = 5
     val totalPrice = applePrice + applesAmount
-    val finalPrice = totalPrice + 5
-    println(finalPrice)
+    println(totalPrice)
 }
