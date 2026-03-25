bd60cd90090cfaf37f6c9576989e787135497318
1 <gitigitigiriir@gmail.com>
2026-03-25 12:44:12 +0300

    GS-1-3 maked clear commit

---
+++ src/lesson1/lesson1_task2.kt
@@ -0,0 +1,9 @@
+package lesson1
+
+fun main() {
+    val applePrice : Double = 70000.0
+    val applesAmount : Int = 5
+    val totalPrice = applePrice + applesAmount
+    val finalPrice = totalPrice + 5
+    println(finalPrice)
+}
--- src/lesson1_task2.kt
+++
@@ -1,7 +0,0 @@
-fun main() {
-    val applePrice : Double = 70000.0
-    val applesAmount : Int = 5
-    var totalPrice = applePrice + applesAmount
-    totalPrice += 5
-    println(totalPrice)
-}
